649680e2f41b45a7180a525dfff8ee3b3472fe04
Joni Juntto <bgm043@myy.haaga-helia.fi>
2021-10-11 20:06:13 +0300

    Added flatlist instead of MAP

--- junatMyohassaFront/components/List.js
+++ junatMyohassaFront/components/List.js
@@ -1,18 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from 'react';
 
-export default function List(props){
-    return (
+export default function List(props) {
+
+
+    const Item = ({ title }) => (
         <View>
-            {
-                props.list.map(train => {
-                    return (
-                        <View>
-                            <Text>{train.trainNumber}</Text>
-                        </View>
-                    );
-                })
-            }
+          <Text>{title.commuterLineID}</Text>
         </View>
+      );
+      const renderItem = ({ item }) => (
+        <Item title={item} />
+      );
+    return (
+        <FlatList
+        data={props.list}
+        renderItem={renderItem}
+        keyExtractor={item => item.scheduledTime}
+      />
     );
 }
+
+//Ensin lista josta timetavblerows josta 
+//haetaan kaikki junan pysäkit josta katsotaan missä station shortcode on annetttu parametri
