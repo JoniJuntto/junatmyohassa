403494c67fa068aa1df14bcbce19d39a8f6b1216
Joni Juntto <bgm043@myy.haaga-helia.fi>
2021-10-11 21:17:08 +0300

    More UI changes

--- junatMyohassaFront/screens/Home.js
+++ junatMyohassaFront/screens/Home.js
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import { View, TextInput, Button } from 'react-native';
 import Map from '../components/Map'
 import styles from '../styles/Styles';
+import { SearchBar } from 'react-native-elements';
 
 
 export default function Home({ navigation }) {
 
     const [inputText, setInputText] = useState('')
 
+    
 
     return (
         <View style={styles.container}>
+          
             <TextInput
                 textAlign={'center'}
                 style={styles.input}
@@ -19,7 +22,7 @@ export default function Home({ navigation }) {
                 onChangeText={text => setInputText(text)} value={inputText}
                 placeholder="Find a train or station"
             />
-            
+
             <Button
                 title="Go to stationlisting"
                 onPress={() => {
