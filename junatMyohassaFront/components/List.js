391bbdbcc1e7202a94bc8fedb58789a2f9300a68
thanerva <bgj170@myy.haaga-helia.fi>
2021-10-29 20:11:54 +0300

    kummitusjunien poisto operaatio oli suuri menestys

--- junatMyohassaFront/components/List.js
+++ junatMyohassaFront/components/List.js
@@ -7,29 +7,34 @@ import { useNavigation } from "@react-navigation/native";
 export default function List(props) {
   const navigation = useNavigation();
 
-  const Item = ({ title }) => (
-    <Card containerStyle={styles.card}>
-      <Text>{title.commuterLineid}</Text>
-      <Text>{title.timeTableRows[0].scheduledTime.slice(11, 16)}</Text>
-      <Icon
-        name="info"
-        onPress={() => {
-          navigation.navigate("TrainInfo", {
-            ID: title.commuterLineID,
-            date: title.departureDate,
-            dif: title.scheduledTime,
-            title: title,
-          });
-        }}
-      />
-    </Card>
-  );
+  const Item = ({ title }) => {
+    // Tsiigataan kummitusjunien varalta, eli jos timetablerow on false sitä ei oteta mukaan kaatamaan sovellusta
+    if (Array.isArray(title.timeTableRows) === true) {
+      return (
+        <Card containerStyle={styles.card}>
+          <Text>{title.commuterLineid}</Text>
+          <Text>{title.timeTableRows[0].scheduledTime.slice(11, 16)}</Text>
+          <Icon
+            name="info"
+            onPress={() => {
+              navigation.navigate("TrainInfo", {
+                ID: title.commuterLineid,
+                trainNr: title.trainNumber,
+                dif: title.scheduledTime,
+                title: title,
+              });
+            }}
+          />
+        </Card>
+      );
+    }
+  };
   const renderItem = ({ item }) => <Item title={item} />;
   return (
     <FlatList
       data={props.list}
       renderItem={renderItem}
-      keyExtractor={(item) => item.timeTableRows[0].scheduledTime}
+      keyExtractor={(item) => item.trainNumber.toString()}
     />
   );
 }
--- junatMyohassaFront/screens/TrainInfo.js
+++ junatMyohassaFront/screens/TrainInfo.js
@@ -4,13 +4,13 @@ import { styles } from "../styles/Styles";
 import Map from "../components/Map";
 
 export default function TrainInfo({ navigation, route }) {
-  const { ID, date, title } = route.params;
+  const { ID, trainNr, title } = route.params;
 
   return (
     <View>
       <View>
         <Text>{ID}</Text>
-        <Text>{date}</Text>
+        <Text>{trainNr}</Text>
         <Text>{title.timeTableRows[0].differenceInMinutes}</Text>
         <Text>{title.timeTableRows[0].scheduledTime.slice(11, 16)}</Text>
       </View>
