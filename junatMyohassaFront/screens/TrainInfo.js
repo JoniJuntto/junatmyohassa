46a480cce18960bfb44df0f20f804aba8f6aac87
JoniJuntto <bgm043@myy.haaga-helia.fi>
2021-11-09 20:07:27 +0200

    Ohjeita

--- junatMyohassaFront/screens/TrainInfo.js
+++ junatMyohassaFront/screens/TrainInfo.js
@@ -6,8 +6,11 @@ import Map from "../components/Map";
 export default function TrainInfo({ navigation, route }) {
   const { ID, trainNr, title } = route.params;
 
+  //Funktio, jonka kutsuu nappula, joka lisää train numberin async storageen
+ // Tämän voi toteuttaa samanlailla kuin EditUserInfo.js tehdään aseman kohdalla
   return (
     <View>
+      {/* Tähän nappula josta painamalla käyttäjä valitsee junan suosikkeihin */}
       <View>
         <Text>{ID}</Text>
         <Text>{trainNr}</Text>
